6ee4b69487b9a1a98ae2ff438730583ee920b015
tuanba <tuanbui.10103@gmail.com>
2024-12-17 00:28:56 +0700

    style: update folder public

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 Route::get('/courses', function () {
     return view('courses');
 })->name('courses');
 
-Route::get('/course', function () {
-    return view('course');
-})->name('course');
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/playlist', function () {
+    return view('playlist');
+})->name('playlist');
+
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/teacher_profile', function () {
+    return view('teacher_profile');
+})->name('teacher_profile');
+
+Route::get('/teachers', function () {
+    return view('teachers');
+})->name('teachers');
+
+Route::get('/update', function () {
+    return view('update');
+})->name('update');
+
+Route::get('/watch_video', function () {
+    return view('watch_video');
+})->name('watch_video');
