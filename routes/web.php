1b7057794fc9f131cb4cab696a2b5f458b32cf17
tuanba <tuanbui.10103@gmail.com>
2024-12-17 20:14:00 +0700

    feat: update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use function Ramsey\Uuid\v1;
 
 /*
 |--------------------------------------------------------------------------
@@ -60,3 +63,90 @@ Route::get('/update', function () {
 Route::get('/watch_video', function () {
     return view('watch_video');
 })->name('watch_video');
+
+
+
+
+
+
+
+
+
+
+Route::get('admin/login', function () {
+    return view('admin/login');
+})->name('login');
+
+Route::get('admin/index', function () {
+    return view('admin/index');
+})->name('index');
+
+Route::get('admin/view_teacher', function () {
+    return view('admin/view_teacher');
+})->name('view_teacher');
+
+Route::get('admin/view_courses', function () {
+    return view('admin/view_courses');
+})->name('view_courses');
+
+Route::get('admin/view_user', function () {
+    return view('admin/view_user');
+})->name('view_user');
+
+Route::get('admin/view_playlist', function () {
+    return view('admin/view_playlist');
+})->name('view_playlist');
+
+
+Route::get('admin/update_teacher', function () {
+    return view('admin/update_teacher');
+})->name('update_teacher');
+
+Route::get('admin/update_courses', function () {
+    return view('admin/update_courses');
+})->name('update_courses');
+
+Route::get('admin/update_user', function () {
+    return view('admin/update_user');
+})->name('update_user');
+
+Route::get('admin/update_playlist', function () {
+    return view('admin/update_playlist');
+})->name('update_playlist');
+
+
+
+
+Route::get('admin/view_teacher', function () {
+    return view('admin/view_teacher');
+})->name('delete_teacher');
+
+Route::get('admin/delete_courses', function () {
+    return view('admin/delete_courses');
+})->name('delete_courses');
+
+Route::get('admin/delete_user', function () {
+    return view('admin/delete_user');
+})->name('delete_user');
+
+Route::get('admin/delete_playlist', function () {
+    return view('admin/delete_playlist');
+})->name('delete_playlist');
+
+
+
+Route::get('admin/create_teacher', function () {
+    return view('admin/create_teacher');
+})->name('create_teacher');
+
+Route::get('admin/create_playlist', function () {
+    return view('admin/create_playlist');
+})->name('create_playlist');
+
+Route::get('admin/create_user', function () {
+    return view('admin/create_user');
+})->name('create_user');
+
+Route::get('admin/create_courses', function () {
+    return view('admin/create_courses');
+})->name('create_courses');
